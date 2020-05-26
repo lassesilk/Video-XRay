d0b09fa4a627bbaf011cca81f821412f5d4d14b2
Lasse N Silkoset <lassesilkoset@gmail.com>
2020-05-26 19:11:36 +0200

    Initial Commit

---
+++ Video-XRay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Video-XRay
+//
+//  Created by Lasse Silkoset on 26/05/2020.
+//  Copyright © 2020 Lasse Silkoset. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
